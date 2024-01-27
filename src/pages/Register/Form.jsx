cda6c545e6ed3dd4bbd305e47eb7a9834e414645
Sukkoth <suukootj@gmail.com>
2024-01-27 12:10:52 +0300

    added registraion

--- src/pages/Register/Form.jsx
+++ src/pages/Register/Form.jsx
@@ -1,41 +1,84 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser, setLoading } from "../../redux/features/Auth";
+import RegistrationSchema from "../../utils/yupSchemas/RegistrationSchema";
 import FormGroup from "../Login/FormGroup";
 
 function Form() {
-  function handleLogin(e) {
-    e.preventDefault();
-  }
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, loading: registrationLoading } = useSelector(
+    (state) => state.auth
+  );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(RegistrationSchema),
+  });
+
+  const onSubmitHandler = (data) => {
+    dispatch(setLoading(true));
+    setTimeout(() => {
+      dispatch(registerUser(data));
+      dispatch(setLoading(false));
+    }, 2000);
+  };
+
+  useEffect(() => {
+    if (user?.email) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
   return (
-    <form className='text-start' onSubmit={handleLogin}>
+    <form className='text-start' onSubmit={handleSubmit(onSubmitHandler)}>
       <FormGroup>
         <label htmlFor='username'>Username</label>
         <input
           className='h-14 bg-input-bg px-4 rounded-xl placeholder:text-stone-500  focus:outline-stone-800 active:outline-stone-800 outline-none'
-          type='username'
+          type='text'
           id='username'
           name='username'
           placeholder='Enter username'
+          {...register("username")}
         />
+        <p className='text-sm text-red-300 font-normal'>
+          {errors.username?.message}
+        </p>
       </FormGroup>
       <FormGroup>
         <label htmlFor='firstName'>First Name</label>
         <input
           className='h-14 bg-input-bg px-4 rounded-xl placeholder:text-stone-500  focus:outline-stone-800 active:outline-stone-800 outline-none'
-          type='firstName'
+          type='text'
           id='firstName'
           name='firstName'
           placeholder='Enter first name'
+          {...register("firstName")}
         />
+        <p className='text-sm text-red-300 font-normal'>
+          {errors.firstName?.message}
+        </p>
       </FormGroup>
       <FormGroup>
         <label htmlFor='lastName'>Last Name</label>
         <input
           className='h-14 bg-input-bg px-4 rounded-xl placeholder:text-stone-500  focus:outline-stone-800 active:outline-stone-800 outline-none'
-          type='lastName'
+          type='text'
           id='lastName'
           name='lastName'
           placeholder='Enter last name'
+          {...register("lastName")}
         />
+        <p className='text-sm text-red-300 font-normal'>
+          {errors.lastName?.message}
+        </p>
       </FormGroup>
       <FormGroup>
         <label htmlFor='email'>E-mail</label>
@@ -45,30 +88,45 @@ function Form() {
           id='email'
           name='email'
           placeholder='Enter your email'
+          {...register("email")}
         />
+        <p className='text-sm text-red-300 font-normal'>
+          {errors.email?.message}
+        </p>
       </FormGroup>
       <FormGroup>
-        <label htmlFor='email'>Password</label>
+        <label htmlFor='password'>Password</label>
         <input
           className='h-14 bg-input-bg px-4 rounded-xl placeholder:text-stone-500  focus:outline-stone-800 active:outline-stone-800 outline-none'
-          type='email'
-          id='email'
-          name='email'
+          type='password'
+          id='password'
+          name='password'
           placeholder='Your Password'
+          {...register("password")}
         />
+        <p className='text-sm text-red-300 font-normal'>
+          {errors.password?.message}
+        </p>
       </FormGroup>
       <FormGroup>
-        <label htmlFor='consfirmPassword'>Confirma Password</label>
+        <label htmlFor='confirmPassword'>Confirm Password</label>
         <input
           className='h-14 bg-input-bg px-4 rounded-xl placeholder:text-stone-500  focus:outline-stone-800 active:outline-stone-800 outline-none'
-          type='consfirmPassword'
-          id='consfirmPassword'
-          name='consfirmPassword'
+          type='password'
+          id='confirmPassword'
+          name='confirmPassword'
           placeholder='Confirm password'
+          {...register("confirmPassword")}
         />
+        <p className='text-sm text-red-300 font-normal'>
+          {errors.confirmPassword?.message}
+        </p>
       </FormGroup>
-      <button className='w-full bg-primary-green py-4 rounded-xl mt-12 text-black'>
-        Register
+      <button
+        type='submit'
+        className='w-full bg-primary-green py-4 rounded-xl mt-12 text-black'
+      >
+        {registrationLoading ? "Loading . . ." : "Register"}
       </button>
       <p className='text-center mt-5 text-gray-500'>
         Already have an account?{" "}
---
+++ src/utils/yupSchemas/RegistrationSchema.js
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+
+const RegistrationSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Confirm Password is required"),
+});
+
+export default RegistrationSchema;
